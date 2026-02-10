cceab73a21ae6a8da7471a9001b8de2eacd9d5b6
fafagnon <fafagnonadama@gmail.com>
2026-02-10 16:38:45 +0000

    Système de disponibilité manuelle (Occupé/Libre)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
 module.exports = function (eleventyConfig) {
+    // Filtre pour les dates en français
+    eleventyConfig.addFilter("readableDate", (dateObj) => {
+        if (!dateObj) return "";
+        const date = new Date(dateObj);
+        return new Intl.DateTimeFormat("fr-FR", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+        }).format(date);
+    });
+
     // Copier les dossiers statiques
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
